67aa88f9cd78f35333e73bbecdf278e527b096c6
Ryan Mattson <ryan.mattson32@gmail.com>
2023-10-18 19:57:02 -0600

    ABOutME bugged and delted re add

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <p className="section--title"></p>
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">
+             I currently live in Herriman Utah, I currently work at a local company 
+             Integrated Engineerig, we speaclize in performance parts for Audi & VW's
+            </p>
+            <p className="hero--section-description">
+             I spend most my Free time doing things I enjoy. Such as 
+             playing basketball spending time with family & friends and now coding.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
